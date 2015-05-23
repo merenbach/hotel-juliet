b5114e82d747ac5db7f2ac2814ed2097a66d17bc
Andrew Merenbach <andrew@merenbach.com>
2015-05-23 15:46:51 -0700

    Refactor Vigenere ciphering some more

--- hj/ciphers/substitution/polyalphabetic/vigenere.py
+++ hj/ciphers/substitution/polyalphabetic/vigenere.py
@@ -80,115 +80,120 @@ class VigenereCipher(PolySubCipher):
         """
         return self._transcode(s, reverse=True)
 
+    def _transcode_char(self, passphrase, cipher_func):
+        """ Transcode a character.
+
+        Parameters
+        ----------
+        passphrase : str
+            A passphrase to use for transcoding.
+        cipher_func : function
+            A function (encode or decode) to use.
+
+        Yields
+        -------
+        out : None
+            Waiting for message character.
+        in : str
+            The next message character.
+        out : str
+            The transcoded message character.
+        in : str or None
+            Character to append to keystream, or None to simply append
+            the current keystream character.
+
+        """
+        key = list(passphrase)
+
+        for key_char in key:
+            msg_char = yield
+            transcode_char = cipher_func(msg_char, key_char)
+            if transcode_char:
+                new_kchar = yield transcode_char
+                # append to the keystream either a new character
+                # (in case of autoclave) or the current key character
+                # (in the case of normal transcoding)
+                key.append(new_kchar or key_char)
+            # # if we instead want to provide the used key char...
+            # msg_char = yield key_char
+            # new_kchar = yield cipher_func(msg_char, key_char)
+            # # append to the keystream either a new character
+            # # (in case of autoclave) or the current key character
+            # # (in the case of normal transcoding)
+            # key.append(new_kchar or key_char)
+            # ## this might or might not be more robust or performant;
+            # ## it won't append to the key (even for default behavior
+            # ## of cycling key) unless the current key character
+            # ## was in the list of keys for the tableau
+
+    def _transcode_message(self, message, reverse):
+        _encoding_text_autoclave = self.text_autoclave and not reverse
+        _decoding_text_autoclave = self.text_autoclave and reverse
+        _encoding_key_autoclave = self.key_autoclave and not reverse
+        _decoding_key_autoclave = self.key_autoclave and reverse
+
+        append_input = _encoding_text_autoclave or _decoding_key_autoclave
+        append_output = _encoding_key_autoclave or _decoding_text_autoclave
+
+        cipher_func = self.tableau.decode if reverse else self.tableau.encode
+
+        # [TODO] make ordereddict subclass?
+        key_elements = self.tableau.transcoders
+        passphrase = [char for char in self.passphrase if char in key_elements]
+
+        tcoder = self._transcode_char(passphrase, cipher_func)
+        # prime the generator
+        tcoder.send(None)
+
+        for msg_char in message:
+            # [NOTE] this is basically always true if `strict` is on
+            if msg_char in self.tableau.charset:  # is this actually required?
+                # returns None if key char not found in rows
+                tchar = tcoder.send(msg_char)
+
+                if append_input:
+                    autokey_append = msg_char
+                elif append_output:
+                    autokey_append = tchar
+                else:
+                    # normal transcoding
+                    # if autoclave is disabled, this has the nice side
+                    # effect of causing our passphrase to loop indefinitely
+                    # per the default tabula recta encryption behavior
+                    autokey_append = None
+
+                tcoder.send(autokey_append)
+                msg_char = tchar
+            yield msg_char
+
     def _transcode(self, s, reverse=False):
         """ Convert characters from one alphabet to another.
 
         """
-        ### can add to the above
         # Passphrase index: Number of successfully-located characters
         # Used to keep message and passphrase in "synch"
-        # Character n of the message should be transcoded with character (n % passphrase len) of the passphrase
-        encoding_text_autoclave = self.text_autoclave and not reverse
-        decoding_text_autoclave = self.text_autoclave and reverse
-        encoding_key_autoclave = self.key_autoclave and not reverse
-        decoding_key_autoclave = self.key_autoclave and reverse
+        # Character n of the message should be transcoded with character
+        # (n % passphrase len) of the passphrase
 
-        cipher_func = self.tableau.decode if reverse else self.tableau.encode
+        return ''.join(self._transcode_message(s, reverse))
+        #         # alternative tack:
+        # get usable passphrase chars: [p for p in passphrase
+        #                               if p in self.tableau.keys()]
+        # get encodeable characters: [m for m in msg
+        #                             if m in self.tableau.alphabet]
+        # [H, E, L, L, O, ',', ' ', W, O, R, L, D]
+        # [O, C, E, A, N, nil, nil, O, C, E, A, N]
+        # kchars = [n for n in cycle passphrase if tcodeable msg
+        #           at same pos...]
+        # for c, k in zip(encodeable_chars, usable_passphrase_chars):
+        #     output += ...
 
-        def transcode_char(passphrase):
-            """ Transcode a character.
+        # this would include characters that don't belong
+        # if append_input:
+        #     passphrase += s
+        # [NOTE] if passphrase is blank at this point,
+        # the output will be empty.
+        # prime the stream and get our first keychar
 
-            Parameters
-            ----------
-            msg_char : str
-                A message character to transcode.
-            passphrase : str
-                A passphrase to use for transcoding.
-
-            Yields
-            -------
-            out : None
-                Waiting for message character.
-            in : str
-                The next message character.
-            out : str
-                The transcoded message character.
-            in : str or None
-                Character to append to keystream, or None to simply append
-                the current keystream character.
-
-            """
-            key = list(passphrase)
-
-            for key_char in iter(key):
-                msg_char = yield
-                # # if we instead want to provide the used key char...
-                # msg_char = yield key_char
-                # new_kchar = yield cipher_func(msg_char, key_char)
-                # # append to the keystream either a new character
-                # # (in case of autoclave) or the current key character
-                # # (in the case of normal transcoding)
-                # key.append(new_kchar or key_char)
-                ### this might or might not be more robust or performant;
-                ### it won't append to the key (even for default behavior
-                ### of cycling key) unless the current key character
-                ### was in the list of keys for the tableau
-                transcode_char = cipher_func(msg_char, key_char)
-                if transcode_char:
-                    new_kchar = yield transcode_char
-                    # append to the keystream either a new character
-                    # (in case of autoclave) or the current key character
-                    # (in the case of normal transcoding)
-                    key.append(new_kchar or key_char)
-
-
-
-
-#         # alternative tack:
-# get usable passphrase chars: [p for p in passphrase if p in self.tableau.keys()]
-# get encodeable characters: [m for m in msg if m in self.tableau.alphabet]
-# [H, E, L, L, O, ',', ' ', W, O, R, L, D]
-# [O, C, E, A, N, nil, nil, O, C, E, A, N]
-# kchars = [n for n in cycle passphrase if tcodeable msg at same pos...]
-# for c, k in zip(encodeable_chars, usable_passphrase_chars):
-#     output += 
-
-        def tcode(s, passphrase):
-            # this would include characters that don't belong
-            # if encoding_text_autoclave or decoding_key_autoclave:
-            #     passphrase += s
-            key_elements = self.tableau.transcoders # todo make ordereddict subclass?
-            passphrase = [char for char in passphrase if char in key_elements]
-            # [NOTE] if passphrase is blank at this point,
-            # the output will be empty.
-            # prime the stream and get our first keychar
-
-            ### feeding msg to transcoder containing key...
-            ### would other way around be better? feed next key to msg tcoder?
-            tcoder = transcode_char(passphrase)
-            # prime the generator
-            tcoder.send(None)
-            for char in s:
-                # [NOTE] this is basically always true if `strict` is on
-                if char in self.tableau.charset:  # is this actually required?
-                    # returns None if key char not found in rows
-                    tchar = tcoder.send(char)
-
-                    if encoding_text_autoclave or decoding_key_autoclave:
-                        autokey_append = char
-                    elif encoding_key_autoclave or decoding_text_autoclave:
-                        autokey_append = tchar
-                    else:
-                        # normal transcoding
-                        # if autoclave is disabled, this has the nice side
-                        # effect of causing our passphrase to loop indefinitely
-                        # per the default tabula recta encryption behavior
-                        autokey_append = None
-
-                    tcoder.send(autokey_append)
-                    char = tchar
-
-                yield char
-
-        o = [n for n in tcode(s, self.passphrase) if n is not None]
-        return ''.join(o)
+        # ## feeding msg to transcoder containing key...
+        # ## would other way around be better? feed next key to msg tcoder?
