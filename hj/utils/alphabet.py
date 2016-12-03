ea8787e6c44bdd47352be6341dc9a8e69da4a098
Andrew Merenbach <andrew@merenbach.com>
2016-12-02 21:24:57 -0800

    More improvements to Alphabet

--- hj/utils/alphabet.py
+++ hj/utils/alphabet.py
@@ -9,14 +9,37 @@ class Alphabet(UserList):
 
     Parameters
     ----------
-    initstr : str
+    initlist : sequence of str
         An alphabet for use in transcoding.
 
     """
-    def __init__(self, initstr=None):
-        if initstr is None:
-            initstr = string.ascii_uppercase
-        super().__init__(initlist=initstr)
+    def __init__(self, initlist=None):
+        if initlist is None:
+            initlist = string.ascii_uppercase
+        super().__init__(initlist=initlist)
 
     def __str__(self):
         return ''.join(self.data)
+
+    def xtable(self, other):
+        """ Create a translation table mapping this alphabet to another.
+
+        Parameters
+        ----------
+        other : sequence
+            An alphabet to which to map.  If it is not the same leng
+
+        Returns
+        -------
+        out : dict
+            A map of this alphabet to `other`.
+
+        Raises
+        ------
+        ValueError
+            If this alphabet and `other` have different lengths.
+
+        """
+        if len(self.data) != len(other):
+            raise ValueError('alphabet length mismatch')
+        return dict(zip(self.data, other))
