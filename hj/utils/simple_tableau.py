734e5c9191160b7e26f4e10d28083e17575ab06e
Andrew Merenbach <andrew@merenbach.com>
2016-12-11 22:00:01 -0800

    Make SimpleTableau math-based

--- hj/utils/simple_tableau.py
+++ hj/utils/simple_tableau.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from collections import namedtuple
+from . import base
+import itertools
 
 
 class SimpleTableau(namedtuple('SimpleTableau', 'pt ct')):
@@ -53,14 +55,68 @@ class SimpleTableau(namedtuple('SimpleTableau', 'pt ct')):
     def __str__(self):
         return 'PT: {}\nCT: {}'.format(self.pt, self.ct)
 
-    @property
-    def pt2ct(self):
-        """ Create a translation table from plaintext to ciphertext.
+    @staticmethod
+    def translate(e, src, dst, transform=None):
+        """ Another way?
+
+        Parameters
+        ----------
+        e : object
+            An element to transform.
+        src : sequence
+            A source sequence.
+        dst : sequence
+            A destination sequence.
+        transform : callable, optional
+            If supplied, a callable to do additional transforms.
 
         Returns
         -------
-        out : dict
-            A Unicode translation dict.
+        out : object
+            The similarly-positioned counterpart in `dst` of `e` in `src`.
+
+        Raises
+        ------
+        ValueError
+            If `e` is not in `src`.
+        IndexError
+            If `dst` contains no elements.
+
+        Notes
+        -----
+        No checking is done on the lengths of `src` and `dst`.  If they differ
+        or if `src` has repeated elements, the results may not be as expected.
+
+        This is almost equivalent to `dict(zip(src, dst))[e]`, with the
+        added caveat of applying the optional `transform` callable.
+
+        """
+        pos = src.index(e)
+        if callable(transform):
+            pos = transform(pos)
+        return dst[pos % len(dst)]
+
+    def pt2ct(self, e, offset=0):
+        """ Encipher a single element.
+
+        Parameters
+        ----------
+        e : object
+            An element to convert from plaintext to ciphertext.
+        offset : int, optional
+            If supplied, an offset to add.  Default `0`.
+
+        Returns
+        -------
+        out : object
+            The enciphered element, if possible.
+
+        Raises
+        ------
+        ValueError
+            If `e` is not in the plaintext alphabet.
+        IndexError
+            If the ciphertext alphabet is empty.
 
         Notes
         -----
@@ -70,16 +126,29 @@ class SimpleTableau(namedtuple('SimpleTableau', 'pt ct')):
         [TODO] Repeatedly calling this could prove inefficient at scale.
 
         """
-        return self.pt.xtable(self.ct)
+        return self.translate(e, self.pt, self.ct, lambda x: x + offset)
 
-    @property
-    def ct2pt(self):
-        """ Create a translation table from ciphertext to plaintext.
+    def ct2pt(self, e, offset=0):
+        """ Decipher a single element.
+
+        Parameters
+        ----------
+        e : object
+            An element to convert from ciphertext to plaintext.
+        offset : int, optional
+            If supplied, an offset to subtract.  Default `0`.
 
         Returns
         -------
-        out : dict
-            A Unicode translation dict.
+        out : object
+            The enciphered element, if possible.
+
+        Raises
+        ------
+        ValueError
+            If `e` is not in the ciphertext alphabet.
+        IndexError
+            If the plaintext alphabet is empty.
 
         Notes
         -----
@@ -89,4 +158,4 @@ class SimpleTableau(namedtuple('SimpleTableau', 'pt ct')):
         [TODO] Repeatedly calling this could prove inefficient at scale.
 
         """
-        return self.ct.xtable(self.pt)
+        return self.translate(e, self.ct, self.pt, lambda x: x - offset)
