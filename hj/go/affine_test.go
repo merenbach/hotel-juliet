001c02b508b691f895e5540bf5a8702c6388e611
Andrew Merenbach <andrew@merenbach.com>
2018-03-08 21:33:11 -0800

    Update tests for all affine-based ciphers

--- hj/go/affine_test.go
+++ hj/go/affine_test.go
@@ -2,9 +2,6 @@ package main
 
 import "testing"
 
-const MESSAGE_PLAIN = "HELLO, WORLD!"
-const MESSAGE_STRICT = "HELLOWORLD"
-const PASSPHRASE = "OCEANOGRAPHYWHAT"
 const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func runReciprocalTests(t *testing.T, cipher affineCipher, plaintext, ciphertext string) {
@@ -26,6 +23,22 @@ func runReciprocalTests(t *testing.T, cipher affineCipher, plaintext, ciphertext
 	}
 }
 
+func TestAffineCipher(t *testing.T) {
+	tables := []struct{
+		plaintext string
+		ciphertext string
+		a int
+		b int
+	}{
+		{ "HELLO, WORLD!", "AFCCX, BXSCY!", 7, 3 },
+		{ "IPQQJ, ZJCQA!", "HELLO, WORLD!", 7, 3 },
+	}
+	for _, table := range tables {
+		cipher := MakeAffineCipher(ALPHABET, table.a, table.b)
+		runReciprocalTests(t, cipher, table.plaintext, table.ciphertext)
+	}
+}
+
 func TestAtbashCipher(t *testing.T) {
 	tables := []struct{
 		plaintext string
@@ -44,84 +57,45 @@ func TestCaesarCipher(t *testing.T) {
 	tables := []struct{
 		plaintext string
 		ciphertext string
+		b int
 	}{
-		{ "HELLO, WORLD!", "KHOOR, ZRUOG!" },
-		{ "EBIIL, TLOIA!", "HELLO, WORLD!" },
+		{ "HELLO, WORLD!", "KHOOR, ZRUOG!", 3 },
+		{ "EBIIL, TLOIA!", "HELLO, WORLD!", 3 },
+		{ "HELLO, WORLD!", "YVCCF, NFICU!", 17 },
+		{ "QNUUX, FXAUM!", "HELLO, WORLD!", 17 },
 	}
 	for _, table := range tables {
-		cipher := MakeCaesarCipher(ALPHABET, 3)
+		cipher := MakeCaesarCipher(ALPHABET, table.b)
 		runReciprocalTests(t, cipher, table.plaintext, table.ciphertext)
 	}
 }
-/*
-    
 
-self._transcode(c, self.MESSAGE_PLAIN, None, 'SVOOL, DLIOW!', block=None)
-self._transcode(c, self.MESSAGE_PLAIN, self.MESSAGE_STRICT, 'SVOOLDLIOW', block=0)
-self._transcode_reverse(c, self.MESSAGE_PLAIN, None, 'SVOOL, DLIOW!', block=None)
-self._transcode_reverse(c, self.MESSAGE_PLAIN, self.MESSAGE_STRICT, 'SVOOLDLIOW', block=0)
+func TestDecimationCipher(t *testing.T) {
+	tables := []struct{
+		plaintext string
+		ciphertext string
+		a int
+	}{
+		{ "HELLO, WORLD!", "XCZZU, YUPZV!", 7 },
+		{ "BIJJC, SCVJT!", "HELLO, WORLD!", 7 },
+	}
+	for _, table := range tables {
+		cipher := MakeDecimationCipher(ALPHABET, table.a)
+		runReciprocalTests(t, cipher, table.plaintext, table.ciphertext)
+	}
+}
 
-    def _transcode(self, cipher, msg, msg_strict, msg_enc_expected, block):
-        encoded = cipher.encode(msg, block=block)
-        # [TODO] should we test with both strict and non-strict decoding?
-        decoded = cipher.decode(encoded, block=block)
+func TestRot13Cipher(t *testing.T) {
+	tables := []struct{
+		plaintext string
+		ciphertext string
+	}{
+		{ "HELLO, WORLD!", "URYYB, JBEYQ!" },
+		{ "URYYB, JBEYQ!", "HELLO, WORLD!" },
+	}
+	for _, table := range tables {
+		cipher := MakeRot13Cipher(ALPHABET)
+		runReciprocalTests(t, cipher, table.plaintext, table.ciphertext)
+	}
+}
 
-        self.assertEqual(encoded, msg_enc_expected)
-        if msg_enc_expected is not '':
-            self.assertNotEqual(encoded, decoded)
-            self.assertEqual(decoded, block==0 and msg_strict or msg)
-
-    def _transcode_reverse(self, cipher, msg, msg_strict, msg_enc_expected,
-                           block):
-        encoded = cipher.decode(msg, block=block)
-        # [TODO] should we test with both strict and non-strict decoding?
-        decoded = cipher.encode(encoded, block=block)
-
-        self.assertEqual(encoded, msg_enc_expected)
-        if msg_enc_expected is not '':
-            self.assertNotEqual(encoded, decoded)
-            self.assertEqual(decoded, block==0 and msg_strict or msg)
-
-
-    def test_caesarcipher(self):
-        c = CaesarCipher()
-        self._transcode(c, self.MESSAGE_PLAIN, None, 'KHOOR, ZRUOG!', block=None)
-        self._transcode(c, self.MESSAGE_PLAIN, self.MESSAGE_STRICT, 'KHOORZRUOG', block=0)
-        self._transcode_reverse(c, self.MESSAGE_PLAIN, None, 'EBIIL, TLOIA!', block=None)
-        self._transcode_reverse(c, self.MESSAGE_PLAIN, self.MESSAGE_STRICT, 'EBIILTLOIA', block=0)
-
-        c = CaesarCipher(17)
-        self._transcode(c, self.MESSAGE_PLAIN, None, 'YVCCF, NFICU!', block=None)
-        self._transcode(c, self.MESSAGE_PLAIN, self.MESSAGE_STRICT, 'YVCCFNFICU', block=0)
-        self._transcode_reverse(c, self.MESSAGE_PLAIN, None, 'QNUUX, FXAUM!', block=None)
-        self._transcode_reverse(c, self.MESSAGE_PLAIN, self.MESSAGE_STRICT, 'QNUUXFXAUM', block=0)
-
-    def test_keywordcipher(self):
-        c = KeywordCipher('KANGAROO')
-        self._transcode(c, self.MESSAGE_PLAIN, None, 'CRHHL, WLQHG!', block=None)
-        self._transcode(c, self.MESSAGE_PLAIN, self.MESSAGE_STRICT, 'CRHHLWLQHG', block=0)
-        self._transcode_reverse(c, self.MESSAGE_PLAIN, None, 'LJOOF, WFEOI!', block=None)
-        self._transcode_reverse(c, self.MESSAGE_PLAIN, self.MESSAGE_STRICT, 'LJOOFWFEOI', block=0)
-
-    def test_affinecipher(self):
-        c = AffineCipher(7, 3)
-        self._transcode(c, self.MESSAGE_PLAIN, None, 'AFCCX, BXSCY!', block=None)
-        self._transcode(c, self.MESSAGE_PLAIN, self.MESSAGE_STRICT, 'AFCCXBXSCY', block=0)
-        self._transcode_reverse(c, self.MESSAGE_PLAIN, None, 'IPQQJ, ZJCQA!', block=None)
-        self._transcode_reverse(c, self.MESSAGE_PLAIN, self.MESSAGE_STRICT, 'IPQQJZJCQA', block=0)
-
-    def test_decimationcipher(self):
-        c = DecimationCipher(7)
-        self._transcode(c, self.MESSAGE_PLAIN, None, 'XCZZU, YUPZV!', block=None)
-        self._transcode(c, self.MESSAGE_PLAIN, self.MESSAGE_STRICT, 'XCZZUYUPZV', block=0)
-        self._transcode_reverse(c, self.MESSAGE_PLAIN, None, 'BIJJC, SCVJT!', block=None)
-        self._transcode_reverse(c, self.MESSAGE_PLAIN, self.MESSAGE_STRICT, 'BIJJCSCVJT', block=0)
-
-    def test_rot13cipher(self):
-        c = Rot13Cipher()
-        self._transcode(c, self.MESSAGE_PLAIN, None, 'URYYB, JBEYQ!', block=None)
-        self._transcode(c, self.MESSAGE_PLAIN, self.MESSAGE_STRICT, 'URYYBJBEYQ', block=0)
-        self._transcode_reverse(c, self.MESSAGE_PLAIN, None, 'URYYB, JBEYQ!', block=None)
-        self._transcode_reverse(c, self.MESSAGE_PLAIN, self.MESSAGE_STRICT, 'URYYBJBEYQ', block=0)
-
-*/
