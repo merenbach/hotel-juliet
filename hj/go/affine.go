0afd43905ef5c6b9385e0fe5d539611c2e16edb8
Andrew Merenbach <andrew@merenbach.com>
2018-03-08 21:35:34 -0800

    Update todo

--- hj/go/affine.go
+++ hj/go/affine.go
@@ -2,6 +2,19 @@ package main
 
 //todo: strict
 //todo: param groups (caesar, atbash, etc)
+// TODO:
+//  * additional unit tests, esp. with int'l charsets
+//  * should alphabets be specified on encrypt/decrypt or on cipher creation?
+//  * documentation
+//  * formatting
+//  * separate files
+//  * strict mode
+//  * multicase alphabets?
+//  * multi value returns (e.g., out, error)
+//  * keyword cipher
+//  * polyalphabetic ciphers
+//  * should we use pointers instead of values after all?... (probably not)
+//  * also do interface, maybe, for enc/decrypt since not everything is affine based or monoalphabetic
 
 import (
 	"fmt"
