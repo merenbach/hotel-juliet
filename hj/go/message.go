5aa81f3ab103172cf5f0e7396fc1569910501a08
Andrew Merenbach <andrew@merenbach.com>
2018-03-11 17:00:38 -0700

    Flesh out message.go Chunk method

--- hj/go/message.go
+++ hj/go/message.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Cipher interface {
 	Encrypt(string) string
@@ -16,9 +19,21 @@ func (message Message) String() string {
 	return fmt.Sprintf("Message (alphabet=%s): %s", message.Alphabet, message.Text)
 }
 
-/*func (message Message) Chunk(int) Message {
-	a
-}*/
+func (message Message) Chunk(sz int, alphabet string) Message {
+	newRunes := make([]rune, 0)
+	runeCount := 0
+	SPACE := " "
+	for _, c := range []rune(message.Text) {
+		if strings.ContainsRune(alphabet, c) {
+			newRunes = append(newRunes, c)
+			runeCount += 1
+			if runeCount%sz == 0 {
+				newRunes = append(newRunes, []rune(SPACE)[0])
+			}
+		}
+	}
+	return Message{Alphabet: message.Alphabet, Text: string(newRunes)}
+}
 
 func (message Message) Transform(fn func(string) string) Message {
 	return Message{Alphabet: message.Alphabet, Text: fn(message.Text)}
