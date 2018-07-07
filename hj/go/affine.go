34c5bfb32fd7f57fb0e20b87ef8e8ff6e5a4e07f
Andrew Merenbach <andrew@merenbach.com>
2018-07-07 15:30:39 -0700

    Clean up, get closer to hull-dobell

--- hj/go/affine.go
+++ hj/go/affine.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"math/big"
 	"unicode/utf8"
 )
 
+// TODO: cleanup these docs and add some errors
+// affine returns the result of `(ax + b) mod m`
+// TODO: enforce constraints such as m > 0
+// https://en.wikipedia.org/wiki/Linear_congruential_generator
 func MakeSimpleTableauForAffine(ptAlphabet string, a, b int) Cipher {
 	m := utf8.RuneCountInString(ptAlphabet)
 
@@ -36,99 +39,4 @@ func MakeSimpleTableauForDecimation(ptAlphabet string, a int) Cipher {
 
 func MakeSimpleTableauForRot13(ptAlphabet string) Cipher {
 	return MakeSimpleTableauForCaesar(ptAlphabet, 13)
-}
-
-// BELOW IS ALL OLD STUFF; PLEASE REMOVE
-
-// TODO: Needs error option or ok option
-// affine returns the result of `(ax + b) mod m`
-// TODO: enforce constraints such as m > 0
-// https://en.wikipedia.org/wiki/Linear_congruential_generator
-func makeAffine(m, a, b int) func() *big.Int {
-	m_, a_, b_ := int64(m), int64(a), int64(b)
-	// TODO: consider using Hull-Dobell satisfaction to determine if `a` is valid (must be coprime with `m`)
-	f, _ := makeLCG(big.NewInt(m_), big.NewInt(1), big.NewInt(a_), big.NewInt(b_))
-	return f
-}
-
-func makeAffineTableau(alphabet string, a, b int) tableau {
-	ptAlphabet := removeRuneDuplicates([]rune(alphabet))
-	myfn := makeAffine(len(ptAlphabet), a, b)
-	ctAlphabet := removeRuneDuplicates(affineTransform(ptAlphabet, myfn))
-	return tableau{string(ptAlphabet), string(ctAlphabet)}
-}
-
-func MakeAffineEncrypt(alphabet string, a, b int) func(string) string {
-	tableau := makeAffineTableau(alphabet, a, b)
-	return tableau.Pt2Ct()
-}
-
-func MakeAffineDecrypt(alphabet string, a, b int) func(string) string {
-	tableau := makeAffineTableau(alphabet, a, b)
-	return tableau.Ct2Pt()
-}
-
-func MakeAtbashEncrypt(alphabet string) func(string) string {
-	ab := len([]rune(alphabet)) - 1
-	return MakeAffineEncrypt(alphabet, ab, ab)
-}
-func MakeAtbashDecrypt(alphabet string) func(string) string {
-	ab := len([]rune(alphabet)) - 1
-	return MakeAffineDecrypt(alphabet, ab, ab)
-}
-
-func Affine(a, b, m int) func() int {
-	// m_, a_, b_ := int64(m), int64(a), int64(b)
-	// TODO: consider using Hull-Dobell satisfaction to determine if `a` is valid (must be coprime with `m`)
-	for a < 0 {
-		a += m
-	}
-	for b < 0 {
-		b += m
-	}
-	aff, _ := makeLCG2(m, 1, a, b)
-	return aff
-}
-func Atbash(m int) func() int {
-	return Affine(-1, -1, m)
-}
-func Caesar(b int, m int) func() int {
-	return Affine(1, b, m)
-}
-func Decimation(a int, m int) func() int {
-	return Affine(a, 0, m)
-}
-func Rot13(m int) func() int {
-	return Caesar(13, m)
-}
-
-func MakeCaesarEncrypt(alphabet string, b int) func(string) string {
-	return MakeAffineEncrypt(alphabet, 1, b)
-}
-func MakeCaesarDecrypt(alphabet string, b int) func(string) string {
-	return MakeAffineDecrypt(alphabet, 1, b)
-}
-
-func MakeDecimationEncrypt(alphabet string, a int) func(string) string {
-	return MakeAffineEncrypt(alphabet, a, 0)
-}
-func MakeDecimationDecrypt(alphabet string, a int) func(string) string {
-	return MakeAffineDecrypt(alphabet, a, 0)
-}
-
-func MakeRot13Encrypt(alphabet string) func(string) string {
-	return MakeCaesarEncrypt(alphabet, 13)
-}
-func MakeRot13Decrypt(alphabet string) func(string) string {
-	return MakeCaesarDecrypt(alphabet, 13)
-}
-
-// transform transforms a rune array (alphabet) based on a function
-func affineTransform(alphabet []rune, fn func() *big.Int) []rune {
-	out := make([]rune, 0)
-	for range alphabet {
-		pos := fn().Int64()
-		out = append(out, alphabet[pos])
-	}
-	return out
 }
--- hj/go/math.go
+++ hj/go/math.go
@@ -6,37 +6,37 @@ import (
 )
 
 // adapted from: https://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Extended_Euclidean_algorithm#Iterative_algorithm_3
-func xgcd(b, a int) (int, int, int) {
-	x0, x1, y0, y1 := 1, 0, 0, 1
-	for a != 0 {
-		q := b / a
-		b, a = a, b%a
-		x0, x1 = x1, x0-q*x1
-		y0, y1 = y1, y0-q*y1
-	}
-	return b, x0, y0
-}
+// func xgcd(b, a int) (int, int, int) {
+// 	x0, x1, y0, y1 := 1, 0, 0, 1
+// 	for a != 0 {
+// 		q := b / a
+// 		b, a = a, b%a
+// 		x0, x1 = x1, x0-q*x1
+// 		y0, y1 = y1, y0-q*y1
+// 	}
+// 	return b, x0, y0
+// }
 
 // adapted from: https://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Extended_Euclidean_algorithm#Iterative_algorithm_3
 // return (g, x, y) a*x + b*y = gcd(x, y)
-func egcd(a, b int) (int, int, int) {
-	if a == 0 {
-		return b, 0, 1
-	} else {
-		g, x, y := egcd(b%a, a)
-		return g, y - (b/a)*x, x
-	}
-}
+// func egcd(a, b int) (int, int, int) {
+// 	if a == 0 {
+// 		return b, 0, 1
+// 	} else {
+// 		g, x, y := egcd(b%a, a)
+// 		return g, y - (b/a)*x, x
+// 	}
+// }
 
 // adapted from: https://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Extended_Euclidean_algorithm#Iterative_algorithm_3
 // x = mulinv(b) mod n, (x * b) % n == 1
-func mulinv(b, n int) int {
-	g, x, _ := egcd(b, n)
-	if g == 1 {
-		return x % n
-	}
-	return -1
-}
+// func mulinv(b, n int) int {
+// 	g, x, _ := egcd(b, n)
+// 	if g == 1 {
+// 		return x % n
+// 	}
+// 	return -1
+// }
 
 // Modulus returns the remainder of a Euclidean division operation.
 // This works around https://github.com/golang/go/issues/448
@@ -44,16 +44,24 @@ func mulinv(b, n int) int {
 	return ((a % b) + b) % b
 }*/
 
-// Divides tests if a number `b` evenly divides `a`.
-func Divides(a, b *big.Int) bool {
-	return new(big.Int).Mod(a, b).Int64() == 0
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return abs(a)
 }
 
 // Coprime tests if two numbers `a` and `b` are relatively prime.
 // The order of the parameters does not matter.
-func Coprime(a, b *big.Int) bool {
-	gcd := new(big.Int).GCD(nil, nil, a, b)
-	return gcd.Int64() == 1
+func Coprime(a, b int) bool {
+	return gcd(a, b) == 1
 }
 
 func makeLCG2(m, a, c, seed int) (func() int, bool) {
@@ -86,9 +94,9 @@ func makeLCG2(m, a, c, seed int) (func() int, bool) {
 	// A_MINUS_ONE := a - 1
 	// DIVISIBLE_BY_FOUR := func(a int) bool { return a%4 == 0 }
 	switch {
-	// case !Coprime(m, c):
-	// 	fmt.Println("Multiplier and increment should be coprime:", m, c)
-	// 	hull_dobell = false
+	case !Coprime(m, c):
+		fmt.Println("Multiplier and increment should be coprime:", m, c)
+		hull_dobell = false
 	// case !Regular(m, A_MINUS_ONE):
 	// 	fmt.Println("Prime factors of `m` should also divide `a - 1`")
 	// 	hull_dobell = false
@@ -98,36 +106,6 @@ func makeLCG2(m, a, c, seed int) (func() int, bool) {
 	}
 
 	return out, hull_dobell
-}
-
-// TODO: document
-// TODO: TESTS
-// TODO: USE GOROUTINES and channel and then emulate yield in inner func???
-func makeLCG(m, a, c, seed *big.Int) (func() *big.Int, bool) {
-	hull_dobell := true
-
-	A_MINUS_ONE := new(big.Int).Sub(a, big.NewInt(1))
-	DIVISIBLE_BY_FOUR := func(a *big.Int) bool { return Divides(a, big.NewInt(4)) }
-	switch {
-	case !Coprime(m, c):
-		fmt.Println("Multiplier and increment should be coprime:", m, c)
-		hull_dobell = false
-	case !Regular(m, A_MINUS_ONE):
-		fmt.Println("Prime factors of `m` should also divide `a - 1`")
-		hull_dobell = false
-	case DIVISIBLE_BY_FOUR(m) && !DIVISIBLE_BY_FOUR(A_MINUS_ONE):
-		fmt.Println("If 4 divides `m`, 4 should divide `a - 1`")
-		hull_dobell = false
-	}
-
-	newseed := new(big.Int).Mod(seed, m)
-	prevseed := new(big.Int)
-	return func() *big.Int {
-		// seed = (newseed * a + c) % m
-		prevseed.Set(newseed)
-		newseed.Mul(newseed, a).Add(newseed, c).Mod(newseed, m)
-		return prevseed
-	}, hull_dobell
 }
 
 // just return true if either a or b is zero?
