ba7de8c3a032f16814ad90697dd9ca4872ef1d30
Andrew Merenbach <andrew@merenbach.com>
2018-07-10 22:35:32 -0700

    Clean up util

--- hj/go/util.go
+++ hj/go/util.go
@@ -37,34 +37,21 @@ func deduplicateString(s string) string {
 	return out.String()
 }
 
-// // MapRunes transforms a string by mapping runes through a function.
-// func MapRunes(s string, f func(rune) rune) string {
-// 	var out strings.Builder
-// 	for _, r := range []rune(s) {
-// 		o, found := m[r]
-// 		if !found && !strict {
-// 			o = r
-// 		}
-// 		out.WriteRune(o)
-// 	}
-// 	return out.String()
-// }
-
 // zipstrings creates a map from two strings (as rune arrays)
 // zipstrings will panic if any runes in `a` occur more than once in `a`
 // zipstrings will NOT panic if runes repeat in `b`, but often zipstrings will be invoked reciprocally
-func ziprunes(a, b []rune) map[rune]rune {
-	seen := make(map[rune]bool)
-	out := make(map[rune]rune)
-	for i, e := range a {
-		if _, found := seen[e]; found {
-			panic("runes may not repeat")
-		}
-		out[e] = b[i]
-		seen[e] = true
-	}
-	return out
-}
+// func ziprunes(a, b []rune) map[rune]rune {
+// 	seen := make(map[rune]bool)
+// 	out := make(map[rune]rune)
+// 	for i, e := range a {
+// 		if _, found := seen[e]; found {
+// 			panic("runes may not repeat")
+// 		}
+// 		out[e] = b[i]
+// 		seen[e] = true
+// 	}
+// 	return out
+// }
 
 // Backpermute transforms a string based on a generator function.
 // Backpermute will panic if the transform function returns any invalid string index values.
