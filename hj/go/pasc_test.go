ec2195f5f30dc28010098949cf0818eabfb02fd3
Andrew Merenbach <andrew@merenbach.com>
2018-07-17 12:17:44 -0700

    Add example tests for tabula recta output

--- hj/go/pasc_test.go
+++ hj/go/pasc_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 const defaultPolyalphabeticAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
@@ -188,4 +191,198 @@ func TestReverseString(t *testing.T) {
 			t.Errorf("Reverse of string %q was %q; expected %q", k, o, v)
 		}
 	}
+}
+
+func ExampleVigenereCipher() {
+	alphabet := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	c := NewVigenereCipher("", alphabet)
+	fmt.Println(c)
+	// Output:
+	//     A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+	//   +----------------------------------------------------
+	// A | A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+	// B | B C D E F G H I J K L M N O P Q R S T U V W X Y Z A
+	// C | C D E F G H I J K L M N O P Q R S T U V W X Y Z A B
+	// D | D E F G H I J K L M N O P Q R S T U V W X Y Z A B C
+	// E | E F G H I J K L M N O P Q R S T U V W X Y Z A B C D
+	// F | F G H I J K L M N O P Q R S T U V W X Y Z A B C D E
+	// G | G H I J K L M N O P Q R S T U V W X Y Z A B C D E F
+	// H | H I J K L M N O P Q R S T U V W X Y Z A B C D E F G
+	// I | I J K L M N O P Q R S T U V W X Y Z A B C D E F G H
+	// J | J K L M N O P Q R S T U V W X Y Z A B C D E F G H I
+	// K | K L M N O P Q R S T U V W X Y Z A B C D E F G H I J
+	// L | L M N O P Q R S T U V W X Y Z A B C D E F G H I J K
+	// M | M N O P Q R S T U V W X Y Z A B C D E F G H I J K L
+	// N | N O P Q R S T U V W X Y Z A B C D E F G H I J K L M
+	// O | O P Q R S T U V W X Y Z A B C D E F G H I J K L M N
+	// P | P Q R S T U V W X Y Z A B C D E F G H I J K L M N O
+	// Q | Q R S T U V W X Y Z A B C D E F G H I J K L M N O P
+	// R | R S T U V W X Y Z A B C D E F G H I J K L M N O P Q
+	// S | S T U V W X Y Z A B C D E F G H I J K L M N O P Q R
+	// T | T U V W X Y Z A B C D E F G H I J K L M N O P Q R S
+	// U | U V W X Y Z A B C D E F G H I J K L M N O P Q R S T
+	// V | V W X Y Z A B C D E F G H I J K L M N O P Q R S T U
+	// W | W X Y Z A B C D E F G H I J K L M N O P Q R S T U V
+	// X | X Y Z A B C D E F G H I J K L M N O P Q R S T U V W
+	// Y | Y Z A B C D E F G H I J K L M N O P Q R S T U V W X
+	// Z | Z A B C D E F G H I J K L M N O P Q R S T U V W X Y
+}
+
+func ExampleBeaufortCipher() {
+	alphabet := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	c := NewBeaufortCipher("", alphabet)
+	fmt.Println(c)
+	// Output:
+	//     A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+	//   +----------------------------------------------------
+	// Z | Z Y X W V U T S R Q P O N M L K J I H G F E D C B A
+	// Y | Y X W V U T S R Q P O N M L K J I H G F E D C B A Z
+	// X | X W V U T S R Q P O N M L K J I H G F E D C B A Z Y
+	// W | W V U T S R Q P O N M L K J I H G F E D C B A Z Y X
+	// V | V U T S R Q P O N M L K J I H G F E D C B A Z Y X W
+	// U | U T S R Q P O N M L K J I H G F E D C B A Z Y X W V
+	// T | T S R Q P O N M L K J I H G F E D C B A Z Y X W V U
+	// S | S R Q P O N M L K J I H G F E D C B A Z Y X W V U T
+	// R | R Q P O N M L K J I H G F E D C B A Z Y X W V U T S
+	// Q | Q P O N M L K J I H G F E D C B A Z Y X W V U T S R
+	// P | P O N M L K J I H G F E D C B A Z Y X W V U T S R Q
+	// O | O N M L K J I H G F E D C B A Z Y X W V U T S R Q P
+	// N | N M L K J I H G F E D C B A Z Y X W V U T S R Q P O
+	// M | M L K J I H G F E D C B A Z Y X W V U T S R Q P O N
+	// L | L K J I H G F E D C B A Z Y X W V U T S R Q P O N M
+	// K | K J I H G F E D C B A Z Y X W V U T S R Q P O N M L
+	// J | J I H G F E D C B A Z Y X W V U T S R Q P O N M L K
+	// I | I H G F E D C B A Z Y X W V U T S R Q P O N M L K J
+	// H | H G F E D C B A Z Y X W V U T S R Q P O N M L K J I
+	// G | G F E D C B A Z Y X W V U T S R Q P O N M L K J I H
+	// F | F E D C B A Z Y X W V U T S R Q P O N M L K J I H G
+	// E | E D C B A Z Y X W V U T S R Q P O N M L K J I H G F
+	// D | D C B A Z Y X W V U T S R Q P O N M L K J I H G F E
+	// C | C B A Z Y X W V U T S R Q P O N M L K J I H G F E D
+	// B | B A Z Y X W V U T S R Q P O N M L K J I H G F E D C
+	// A | A Z Y X W V U T S R Q P O N M L K J I H G F E D C B
+}
+
+func ExampleGronsfeldCipher() {
+	alphabet := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	c := NewGronsfeldCipher("", alphabet)
+	fmt.Println(c)
+	// Output:
+	//     A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+	//   +----------------------------------------------------
+	// 0 | A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+	// 1 | B C D E F G H I J K L M N O P Q R S T U V W X Y Z A
+	// 2 | C D E F G H I J K L M N O P Q R S T U V W X Y Z A B
+	// 3 | D E F G H I J K L M N O P Q R S T U V W X Y Z A B C
+	// 4 | E F G H I J K L M N O P Q R S T U V W X Y Z A B C D
+	// 5 | F G H I J K L M N O P Q R S T U V W X Y Z A B C D E
+	// 6 | G H I J K L M N O P Q R S T U V W X Y Z A B C D E F
+	// 7 | H I J K L M N O P Q R S T U V W X Y Z A B C D E F G
+	// 8 | I J K L M N O P Q R S T U V W X Y Z A B C D E F G H
+	// 9 | J K L M N O P Q R S T U V W X Y Z A B C D E F G H I
+}
+
+func ExampleTrithemiusCipher() {
+	alphabet := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	c := NewTrithemiusCipher(alphabet)
+	fmt.Println(c)
+	// Output:
+	//     A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+	//   +----------------------------------------------------
+	// A | A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+	// B | B C D E F G H I J K L M N O P Q R S T U V W X Y Z A
+	// C | C D E F G H I J K L M N O P Q R S T U V W X Y Z A B
+	// D | D E F G H I J K L M N O P Q R S T U V W X Y Z A B C
+	// E | E F G H I J K L M N O P Q R S T U V W X Y Z A B C D
+	// F | F G H I J K L M N O P Q R S T U V W X Y Z A B C D E
+	// G | G H I J K L M N O P Q R S T U V W X Y Z A B C D E F
+	// H | H I J K L M N O P Q R S T U V W X Y Z A B C D E F G
+	// I | I J K L M N O P Q R S T U V W X Y Z A B C D E F G H
+	// J | J K L M N O P Q R S T U V W X Y Z A B C D E F G H I
+	// K | K L M N O P Q R S T U V W X Y Z A B C D E F G H I J
+	// L | L M N O P Q R S T U V W X Y Z A B C D E F G H I J K
+	// M | M N O P Q R S T U V W X Y Z A B C D E F G H I J K L
+	// N | N O P Q R S T U V W X Y Z A B C D E F G H I J K L M
+	// O | O P Q R S T U V W X Y Z A B C D E F G H I J K L M N
+	// P | P Q R S T U V W X Y Z A B C D E F G H I J K L M N O
+	// Q | Q R S T U V W X Y Z A B C D E F G H I J K L M N O P
+	// R | R S T U V W X Y Z A B C D E F G H I J K L M N O P Q
+	// S | S T U V W X Y Z A B C D E F G H I J K L M N O P Q R
+	// T | T U V W X Y Z A B C D E F G H I J K L M N O P Q R S
+	// U | U V W X Y Z A B C D E F G H I J K L M N O P Q R S T
+	// V | V W X Y Z A B C D E F G H I J K L M N O P Q R S T U
+	// W | W X Y Z A B C D E F G H I J K L M N O P Q R S T U V
+	// X | X Y Z A B C D E F G H I J K L M N O P Q R S T U V W
+	// Y | Y Z A B C D E F G H I J K L M N O P Q R S T U V W X
+	// Z | Z A B C D E F G H I J K L M N O P Q R S T U V W X Y
+}
+
+func ExampleVariantBeaufortCipher() {
+	alphabet := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	c := NewVariantBeaufortCipher("", alphabet)
+	fmt.Println(c)
+	// Output:
+	//     Z Y X W V U T S R Q P O N M L K J I H G F E D C B A
+	//   +----------------------------------------------------
+	// A | Z Y X W V U T S R Q P O N M L K J I H G F E D C B A
+	// B | Y X W V U T S R Q P O N M L K J I H G F E D C B A Z
+	// C | X W V U T S R Q P O N M L K J I H G F E D C B A Z Y
+	// D | W V U T S R Q P O N M L K J I H G F E D C B A Z Y X
+	// E | V U T S R Q P O N M L K J I H G F E D C B A Z Y X W
+	// F | U T S R Q P O N M L K J I H G F E D C B A Z Y X W V
+	// G | T S R Q P O N M L K J I H G F E D C B A Z Y X W V U
+	// H | S R Q P O N M L K J I H G F E D C B A Z Y X W V U T
+	// I | R Q P O N M L K J I H G F E D C B A Z Y X W V U T S
+	// J | Q P O N M L K J I H G F E D C B A Z Y X W V U T S R
+	// K | P O N M L K J I H G F E D C B A Z Y X W V U T S R Q
+	// L | O N M L K J I H G F E D C B A Z Y X W V U T S R Q P
+	// M | N M L K J I H G F E D C B A Z Y X W V U T S R Q P O
+	// N | M L K J I H G F E D C B A Z Y X W V U T S R Q P O N
+	// O | L K J I H G F E D C B A Z Y X W V U T S R Q P O N M
+	// P | K J I H G F E D C B A Z Y X W V U T S R Q P O N M L
+	// Q | J I H G F E D C B A Z Y X W V U T S R Q P O N M L K
+	// R | I H G F E D C B A Z Y X W V U T S R Q P O N M L K J
+	// S | H G F E D C B A Z Y X W V U T S R Q P O N M L K J I
+	// T | G F E D C B A Z Y X W V U T S R Q P O N M L K J I H
+	// U | F E D C B A Z Y X W V U T S R Q P O N M L K J I H G
+	// V | E D C B A Z Y X W V U T S R Q P O N M L K J I H G F
+	// W | D C B A Z Y X W V U T S R Q P O N M L K J I H G F E
+	// X | C B A Z Y X W V U T S R Q P O N M L K J I H G F E D
+	// Y | B A Z Y X W V U T S R Q P O N M L K J I H G F E D C
+	// Z | A Z Y X W V U T S R Q P O N M L K J I H G F E D C B
+}
+
+func ExampleDellaPortaCipher() {
+	alphabet := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	c := NewDellaPortaCipher("", alphabet)
+	fmt.Println(c)
+	// Output:
+	//     A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+	//   +----------------------------------------------------
+	// A | N O P Q R S T U V W X Y Z A B C D E F G H I J K L M
+	// B | N O P Q R S T U V W X Y Z A B C D E F G H I J K L M
+	// C | O P Q R S T U V W X Y Z N M A B C D E F G H I J K L
+	// D | O P Q R S T U V W X Y Z N M A B C D E F G H I J K L
+	// E | P Q R S T U V W X Y Z N O L M A B C D E F G H I J K
+	// F | P Q R S T U V W X Y Z N O L M A B C D E F G H I J K
+	// G | Q R S T U V W X Y Z N O P K L M A B C D E F G H I J
+	// H | Q R S T U V W X Y Z N O P K L M A B C D E F G H I J
+	// I | R S T U V W X Y Z N O P Q J K L M A B C D E F G H I
+	// J | R S T U V W X Y Z N O P Q J K L M A B C D E F G H I
+	// K | S T U V W X Y Z N O P Q R I J K L M A B C D E F G H
+	// L | S T U V W X Y Z N O P Q R I J K L M A B C D E F G H
+	// M | T U V W X Y Z N O P Q R S H I J K L M A B C D E F G
+	// N | T U V W X Y Z N O P Q R S H I J K L M A B C D E F G
+	// O | U V W X Y Z N O P Q R S T G H I J K L M A B C D E F
+	// P | U V W X Y Z N O P Q R S T G H I J K L M A B C D E F
+	// Q | V W X Y Z N O P Q R S T U F G H I J K L M A B C D E
+	// R | V W X Y Z N O P Q R S T U F G H I J K L M A B C D E
+	// S | W X Y Z N O P Q R S T U V E F G H I J K L M A B C D
+	// T | W X Y Z N O P Q R S T U V E F G H I J K L M A B C D
+	// U | X Y Z N O P Q R S T U V W D E F G H I J K L M A B C
+	// V | X Y Z N O P Q R S T U V W D E F G H I J K L M A B C
+	// W | Y Z N O P Q R S T U V W X C D E F G H I J K L M A B
+	// X | Y Z N O P Q R S T U V W X C D E F G H I J K L M A B
+	// Y | Z N O P Q R S T U V W X Y B C D E F G H I J K L M A
+	// Z | Z N O P Q R S T U V W X Y B C D E F G H I J K L M A
 }
