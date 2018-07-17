82060b0a8f51449fbcd031373630f67355c96385
Andrew Merenbach <andrew@merenbach.com>
2018-07-17 12:50:18 -0700

    Clarify wrapString

--- hj/go/tabularecta.go
+++ hj/go/tabularecta.go
@@ -133,9 +133,8 @@ func (tr *TabulaRecta) Decipher(s string, strict bool) string {
 // WrapString will error out if the provided offset is negative.
 func wrapString(s string, i int) string {
 	// if we simply `return s[i:] + s[:i]`, we're operating on bytes, not runes
-	u := []rune(s)
-	v := append(u[i:], u[:i]...)
-	return string(v)
+	rr := []rune(s)
+	return string(rr[i:]) + string(rr[:i])
 }
 
 // WrapString wraps a string a specified number of indices.
